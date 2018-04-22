3438b3c695061d9957ad948f8267499b5c5876de
devonbahary <devonbahary@gmail.com>
2018-04-22 11:38:32 -0700

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Home = () => (
+    <div>
+        <h1>React Starter Template</h1>
+        <h3>This is my starting template for a React project!</h3>
+        <ul>
+            <li>React Router</li>
+            <li>SASS</li>
+            <li>Webpack configured for development</li>
+            <li>Babel plugins:</li>
+            <ul>
+                <a href='https://babeljs.io/docs/plugins/transform-class-properties/'><li>Class properties transform</li></a>
+                <a href='https://babeljs.io/docs/plugins/transform-object-rest-spread/'><li>Object rest spread transform</li></a>
+            </ul>
+        </ul>
+    </div>
+);
+
+export default Home;
---
+++ src/routers/AppRouter.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from '../components/Home';
+
+const AppRouter = () => (
+    <BrowserRouter>
+        <div>
+            <Switch>
+                <Route to='/' exact component={Home} />
+            </Switch>
+        </div>
+    </BrowserRouter>
+);
+
+export default AppRouter;
